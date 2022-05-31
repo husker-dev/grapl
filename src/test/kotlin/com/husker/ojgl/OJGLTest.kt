c4c74fc386e4797b913b4f877de9eb5a2f0014d1
Nikita Shtengauer <redfancoestar@gmail.com>
2022-05-31 15:17:31 +0300

    Test changes

--- src/test/kotlin/com/husker/ojgl/OJGLTest.kt
+++ src/test/kotlin/com/husker/ojgl/OJGLTest.kt
@@ -1,34 +1,28 @@
 package com.husker.ojgl
 
 import com.huskerdev.ojgl.GLContext
+import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
-import org.lwjgl.opengl.GL
 
 class OJGLTest {
 
     @Test
     fun createInstance() {
-        GLContext.createNew().makeCurrent()
-
-        GL.createCapabilities()
+        assertEquals(GLContext.createNew().makeCurrent(), true)
     }
 
     @Test
     fun createShared() {
         val first = GLContext.createNew()
         val second = GLContext.createNew(first)
-        second.makeCurrent()
-
-        GL.createCapabilities()
+        assertEquals(second.makeCurrent(), true)
     }
 
     @Test
     fun fromCurrent() {
         GLContext.createNew().makeCurrent()
         val second = GLContext.fromCurrent()
-        second.makeCurrent()
-
-        GL.createCapabilities()
+        assertEquals(second.makeCurrent(), true)
     }
 
 }
