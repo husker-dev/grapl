a79da2f261a398b12486e5916e87af6bb710073d
husker-dev <redfancoestar@gmail.com>
2023-09-06 13:18:18 +0300

    Some project fixes

--- src/main/kotlin/com/huskerdev/ojgl/GLPlatform.kt
+++ src/main/kotlin/com/huskerdev/ojgl/GLPlatform.kt
@@ -2,42 +2,25 @@ package com.huskerdev.ojgl
 
 import com.huskerdev.ojgl.platforms.*
 import com.huskerdev.ojgl.utils.*
-import java.io.FileOutputStream
 
 abstract class GLPlatform {
 
     companion object {
         init {
-            val arch = System.getProperty("os.arch").lowercase().run {
-                if (startsWith("aarch64") || startsWith("armv8")) "arm64"
-                else if (startsWith("arm")) "arm32"
-                else if ("64" in this) "x64"
-                else "x86"
-            }
-
-            val basename = "ojgl"
+            val basename = "offscreen-jgl"
             val fileName = when(PlatformUtils.os) {
-                Windows -> "${basename}-${arch}.dll"
-                Linux -> "${basename}-${arch}.so"
-                MacOS -> "${basename}.dylib"
+                OS.Windows, OS.Linux    -> "$basename-${PlatformUtils.arch}.${PlatformUtils.dynamicLibExt}"
+                OS.MacOS                -> "$basename.dylib"
                 else -> throw UnsupportedOperationException("Unsupported OS")
             }
-
-            val tmpFileName = "${System.getProperty("java.io.tmpdir")}/$fileName"
-            try {
-                val inputStream = this::class.java.getResourceAsStream("/com/huskerdev/ojgl/natives/$fileName")!!
-                FileOutputStream(tmpFileName).use {
-                    inputStream.copyTo(it)
-                }
-            }catch (_: Exception){}
-            System.load(tmpFileName)
+            PlatformUtils.loadLibraryFromResources("/com/huskerdev/ojgl/natives/$fileName")
         }
 
         val current by lazy {
             when(PlatformUtils.os) {
-                Windows -> WinGLPlatform()
-                Linux -> LinuxGLPlatform()
-                MacOS -> MacGLPlatform()
+                OS.Windows  -> WinGLPlatform()
+                OS.Linux    -> LinuxGLPlatform()
+                OS.MacOS    -> MacGLPlatform()
                 else -> throw UnsupportedOperationException("Unsupported OS")
             }
         }
--- src/main/kotlin/com/huskerdev/ojgl/utils/PlatformUtils.kt
+++ src/main/kotlin/com/huskerdev/ojgl/utils/PlatformUtils.kt
@@ -1,19 +1,57 @@
 package com.huskerdev.ojgl.utils
 
-const val Other = -1
-const val Windows = 0
-const val Linux = 1
-const val MacOS = 2
+import java.io.FileOutputStream
+
+enum class OS(val displayName: String) {
+    Other("unknown"),
+    Windows("windows"),
+    Linux("linux"),
+    MacOS("macos");
+    override fun toString() = displayName
+}
+
+enum class Architecture(val displayName: String) {
+    ARM64("arm64"),
+    ARM32("arm32"),
+    X64("x64"),
+    X86("x86");
+    override fun toString() = displayName
+}
+
 
 class PlatformUtils {
 
     companion object {
         val os = System.getProperty("os.name", "generic").lowercase().run {
-            return@run if ("mac" in this || "darwin" in this) MacOS
-            else if ("win" in this) Windows
-            else if ("nux" in this || "nix" in this || "aix" in this) Linux
-            else Other
+            return@run if ("mac" in this || "darwin" in this) OS.MacOS
+            else if ("win" in this) OS.Windows
+            else if ("nux" in this || "nix" in this || "aix" in this) OS.Linux
+            else OS.Other
+        }
+
+        val arch = System.getProperty("os.arch").lowercase().run {
+            if (startsWith("aarch64") || startsWith("armv8")) Architecture.ARM64
+            else if (startsWith("arm")) Architecture.ARM32
+            else if ("64" in this) Architecture.X64
+            else Architecture.X86
+        }
+
+        val dynamicLibExt = when(os){
+            OS.Other -> throw UnsupportedOperationException("Can not get library extension for unsupported OS")
+            OS.Windows -> "dll"
+            OS.Linux -> "so"
+            OS.MacOS -> "dylib"
+        }
+
+        fun loadLibraryFromResources(path: String) {
+            var fileName = path.replace("/", "-")
+            if(fileName.startsWith("-")) fileName = fileName.substring(1)
+
+            val tmpFilePath = "${System.getProperty("java.io.tmpdir")}$fileName"
+            FileOutputStream(tmpFilePath).use {
+                this::class.java.getResourceAsStream(path)!!.copyTo(it)
+            }
+            System.load(tmpFilePath)
         }
     }
-
 }
